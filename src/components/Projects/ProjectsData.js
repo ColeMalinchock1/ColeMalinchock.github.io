3509a1c46c7be4184a11243be842a32fed11664a
Cole Malinchock <chmalinc@ncsu.edu>
2025-05-19 21:12:19 -0400

    New updates

--- src/components/Projects/ProjectsData.js
+++ src/components/Projects/ProjectsData.js
@@ -15,36 +15,94 @@ const projects = [
         "/images/projects/Wheelchair_CAD.png",
         "/images/projects/Walker_And_Wheelchair.png"
       ],
-      tags: ["SolidWorks", "Python", "KiCAD"],
+      tags: ["SolidWorks", "Python","Kivy" ,"KiCAD"],
       links: {
         github: "https://github.com/InsightNCSU",
-        live: "https://drive.google.com/file/d/1EN9imIc2E_8eF6kK3yJAMlcxjQkyRCB7/view?usp=sharing",
-        cad: "https://cad-link.com",
-        pcb: "https://pcb-design-link.com"
+        live: "https://youtu.be/9lVFlevcwTU",
+        cad: "https://github.com/InsightNCSU/CAD",
+        pcb: "https://github.com/InsightNCSU/PCB/tree/main"
       }
     },
     {
       id: 2,
-      title: "Project Name 1",
-      shortDescription: "Brief description of the project",
-      fullDescription: "Detailed description of the project and its features. Include technical details, challenges overcome, and outcomes achieved.",
+      title: "OVAL",
+      shortDescription: "On-campus Vehicle Autonomous Launch",
+      fullDescription: "A general service autonomous robot to be utilized for research and student support on campuses.",
       // Main image (used for card)
-      image: "./images/Project1_Main.png",
+      image: "./images/projects/OVAL_Vehicle.png",
       // Array of all project images
       images: [
-        "./images/Project1_Main.png",
-        "./images/Project1_Detail.png",
-        "./images/Project1_Diagram.png"
+        "./images/projects/OVAL_Vehicle.png",
+        "./images/projects/OVAL_Driving.png",
+        "./images/projects/OVAL_Mockup.png"
       ],
-      tags: ["React", "Node.js", "MongoDB"],
+      tags: ["SolidWorks", "ROS2", "Python", "C++"],
       links: {
         github: "https://github.com/yourusername/project1",
-        live: "https://project-demo.com",
-        cad: "https://cad-link.com",
-        pcb: "https://pcb-design-link.com"
+        live: "https://youtube.com/shorts/7gE9keZnb2I?feature=share",
+        cad: "https://cad-link.com"
       }
     },
-    // Add more projects here
+    {
+        id: 3,
+        title: "FWC sUAV",
+        shortDescription: "Florida Fish & Wildlife Conservation Commission Small Unmanned Aerial Vehicle",
+        fullDescription: "Undergraduate research assistant to develop the software to enable to drone to autonomously avoid obstacles as it travels between waypoints.",
+        // Main image (used for card)
+        image: "./images/projects/FWC.png",
+        // Array of all project images
+        images: [
+            "./images/projects/FWC.png",
+            "./images/projects/FWC.png",
+            "./images/projects/FWC.png"
+        ],
+        tags: ["ROS2", "Python", "MuJoCo", "Reinforcement Learning"],
+        links: {
+            github: "https://github.com/yourusername/project1",
+            live: "https://youtube.com/shorts/7gE9keZnb2I?feature=share",
+            cad: "https://cad-link.com"
+        }
+    },
+    {
+        id: 4,
+        title: "Autonomous Vehicle Development",
+        shortDescription: "Design and develop the 1/16th scale autonomous vehicle to be used in NCSU CSC495 course.",
+        fullDescription: "Developed the 1/16th scale autonomous vehicles from the base of a Traxxas RC car and developing the mounting of an NVIDIA Jetson Orin, RPlidar, Zed2i, Teensy 4.1, and Hobbywing brushless motor.",
+        // Main image (used for card)
+        image: "./images/projects/CSC495.png",
+        // Array of all project images
+        images: [
+            "./images/projects/CSC495.png",
+            "./images/projects/CSC495.png",
+            "./images/projects/CSC495.png"
+        ],
+        tags: ["ROS2", "KiCAD", "Python", "C++", "SolidWorks"],
+        links: {
+            github: "https://github.com/yourusername/project1",
+            live: "https://youtube.com/shorts/7gE9keZnb2I?feature=share",
+            cad: "https://cad-link.com"
+        }
+    },
+    {
+        id: 5,
+        title: "AAV",
+        shortDescription: "Autonomous Aquatic Vessel.",
+        fullDescription: "Personal project to develop an autonomous vessel that is capable of traversing through a body of water to record relevant data on water temperature, turbidity, temperature, and depth.",
+        // Main image (used for card)
+        image: "./images/projects/AAV.png",
+        // Array of all project images
+        images: [
+            "./images/projects/AAV.png",
+            "./images/projects/AAV.png",
+            "./images/projects/AAV.png"
+        ],
+        tags: ["ROS2", "KiCAD", "Python", "C++", "SolidWorks", "Fiber Glass Laying"],
+        links: {
+            github: "https://github.com/yourusername/project1",
+            live: "https://youtube.com/shorts/7gE9keZnb2I?feature=share",
+            cad: "https://cad-link.com"
+        }
+    },
   ];
   
   export default projects;
